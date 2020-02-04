c66a44b8132e7b9105ba23a9ed45000e84b19680
Joe Meilinger <joemeilinger@gmail.com>
2020-02-03 22:07:32 -0600

    Fleshed out example app and slides

--- c1/app.py
+++ c1/app.py
@@ -1,15 +1,26 @@
 import os
+import requests
 from flask import Flask
-from flask_restful import Resource, Api
+from flask_restful import Resource, Api, reqparse, abort
+from logger import logger
 
 app = Flask(__name__)
 api = Api(app)
 
-class HelloWorld(Resource):
-    def get(self):
-        return {'Hello': 'World'}
+parser = reqparse.RequestParser()
+parser.add_argument('name', help='Make and model of car')
 
-api.add_resource(HelloWorld, '/')
+# Our client (external) API
+class CreateCar(Resource):
+    def post(self):
+        args = parser.parse_args(strict=True)
+        logger.info(f'c1: Received request to create car: {args["name"]}')
+        resp = requests.post('http://c2:5001/', data={'name': args['name']})
+        return resp.json(), resp.status_code
+
+
+api.add_resource(CreateCar, '/')
 
 if __name__ == '__main__':
-    app.run(port=os.getenv('PORT', 5000))
+    # NOTE: you MUST bind to 0.0.0.0 inside the container
+    app.run(host='0.0.0.0', port=os.getenv('PORT', 5000))
--- c2/app.py
+++ c2/app.py
@@ -1,21 +1,23 @@
 import os
 from flask import Flask
-from flask_restful import Resource, Api
+from flask_restful import Resource, Api, reqparse
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import ForeignKey
 from sqlalchemy.sql import func
 from sqlalchemy.orm import relationship
 from logger import logger
 
+# Flask/SQLAlchemy setup and config
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URI', 'sqlite:////tmp/test.db')
 api = Api(app)
 db = SQLAlchemy(app)
 
+# ORM - Cars and Car components
 class Car(db.Model):
     __tablename__ = 'cars'
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(80), unique=True, nullable=False)
+    name = db.Column(db.String(80), nullable=False)
     created_at = db.Column(db.DateTime, server_default=func.now())
     components = relationship('CarComponent', back_populates='car')
 
@@ -27,13 +29,30 @@ class CarComponent(db.Model):
     created_at = db.Column(db.DateTime, server_default=func.now())
     car = relationship('Car', back_populates='components')
 
+parser = reqparse.RequestParser()
+parser.add_argument('name')
+
+# Our internal car creation API
 class CarComponentCreator(Resource):
-    def get(self):
-        logger.info('Hey there')
-        return {'Hello': 'World'}
+    def post(self):
+        args = parser.parse_args(strict=True)
+        logger.info(f'c2: Received request to extrapolate car components for {args["name"]}')
+        car = Car(name=args['name'])
+        engine = CarComponent(name='engine', car=car)
+        wheels = CarComponent(name='wheels', car=car)
+        body = CarComponent(name='body', car=car)
+        db.session.add(car)
+        db.session.add(engine)
+        db.session.add(wheels)
+        db.session.add(body)
+        db.session.commit()
+        db.session.refresh(car)
+        logger.info(f'c2: Car created {car.name} -- id: {car.id} with components')
+        return {'created': True, 'car.id': car.id}
 
 api.add_resource(CarComponentCreator, '/')
 
 if __name__ == '__main__':
     db.create_all()
-    app.run(port=os.getenv('PORT', 5001))
+    # NOTE: you MUST bind to 0.0.0.0 inside the container
+    app.run(host='0.0.0.0', port=os.getenv('PORT', 5001))
